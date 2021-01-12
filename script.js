9d39c66107433c5031746ff27f4a32cb5f04b9fb
Dorrian Weber <dorrianweber@gmail.com>
2021-01-11 18:56:10 -0800

    added arrays for upper case letters & numbers

--- script.js
+++ script.js
@@ -10,9 +10,18 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var specialChars = ["~","!","#","$","%","^","&","*","(",")","_","-","+","=","`","<",",",">",".","?","/","{","}","[","]","|"];
+
+// Arrays for different sets of characters
 
   var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  var specialChars = ["~","!","#","$","%","^","&","*","(",")","_","-","+","=","`","<",",",">",".","?","/","{","}","[","]","|"];
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
   var myPassword = ""
 
@@ -25,9 +34,9 @@ function generatePassword() {
 
   var pwLowercase = confirm("Include lowercase characters?");
 
-  if (pwLowercase === true) {
-    myPassword
-  }
+  // if (pwLowercase === true) {
+  //   myPassword = myPassword + randomLowercase
+  // }
 
   var pwUppercase = confirm("Include uppercase characters?");
   var pwNumeric = confirm("Include numbers?");
@@ -40,9 +49,15 @@ function generatePassword() {
 
   for (var i = 0; i < pwlength; i++) {
     var randomIndex = Math.floor(Math.random() * specialChars.length);
-    var randomChar = specialChars[randomIndex];
-    myPassword = myPassword + randomChar;
+    var randomSpecial = specialChars[randomIndex];
+    myPassword = myPassword + randomSpecial;
   }
+
+  // for (var i = 0; i < pwlength; i++) {
+  //   var randomIndex = Math.floor(Math.random() * lowercaseChar.length);
+  //   var randomLowercase = lowercaseChars[randomIndex];
+  //   myPassword = myPassword + randomLowercase;
+  // }
 
   return myPassword;
 }
